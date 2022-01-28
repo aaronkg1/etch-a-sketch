4135d7b7cfcb057c1a6698620a9ff06610a0518d
aaronkg1 <aaronkg1@gmail.com>
2022-01-28 12:28:27 +0000

    Fixed problem of css precedence creating bug. Added a rainbow drawing option

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const purpleButton = document.querySelector('#purple');
 const orangeButton = document.querySelector('#orange');
 const blueButton = document.querySelector('#blue');
 const colorButtons = document.querySelectorAll('.color-button');
+const cssSelector = document.querySelector('link');
+const rainbow = document.querySelector('#rainbow');
+
+let color;
 
 function makeGrid(x, y) {
     for (let i = 0; i < x; i++) {
@@ -17,41 +21,44 @@ function makeGrid(x, y) {
             const box = document.createElement('div');
             box.className = 'box';
             row.appendChild(box);
-            
+
         }
         container.appendChild(row);
-    } 
+    }
 }
 
 makeGrid(50, 60);
 
 clearButton.addEventListener('click', () => {
-    boxes.forEach(box => box.classList.remove('blue', 'orange', 'purple', 'green', 'red', 'black'))
+    boxes.forEach(box => box.style.background = 'white');
 });
 
 const boxes = document.querySelectorAll('.box');
 
 
 
-colorButtons.forEach(button => button.addEventListener('click', (e) => {
-    const color = e.target.id;
-    
-  boxes.forEach(box => box.addEventListener('mouseover', addClass));
 
-    
 
-    function addClass() {
-    
-    this.classList.toggle(color);
-};
+colorButtons.forEach(button => button.addEventListener('click', (event) => {
+    if (event.target.id == 'rainbow') {
+        boxes.forEach(box => box.addEventListener('mouseover', randomRgb));
+    }
+  
+    else
+        boxes.forEach(box => box.removeEventListener('mouseover', randomRgb));
+        color = event.target.id;
+
 
 }));
+boxes.forEach(box => box.addEventListener('mouseover', addClass,));
 
+function addClass() {
+    this.style.background = color;
+};
 
-
-
-
-// function() {
-    
-//     this.classList.add('black');
-// }
+function randomRgb() {
+    let red = Math.floor(Math.random() * 255) + 1;
+    let green = Math.floor(Math.random() * 255) + 1;
+    let blue = Math.floor(Math.random() * 255) + 1;
+    color = `rgb(${red}, ${green}, ${blue})`;
+}
