62e06ce39298adc5c7eca69fe1d36c9f21c14689
aaronkg1 <aaronkg1@gmail.com>
2022-01-28 14:34:36 +0000

    Added the option to change the background color

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ const purpleButton = document.querySelector('#purple');
 const orangeButton = document.querySelector('#orange');
 const blueButton = document.querySelector('#blue');
 const colorButtons = document.querySelectorAll('.color-button');
+const backgroundButtons = document.querySelectorAll('.background-button')
 const cssSelector = document.querySelector('link');
 const rainbow = document.querySelector('#rainbow');
-
+const rowSize = document.querySelector('#rowsize');
+const columnSize = document.querySelector('columnsize');
+const generateButton = document.querySelector('#generate');
 let color;
+
+let rowSizeValue = document.querySelector('#rowsizevalue').textContent;
+let columnSizeValue = document.querySelector('#columnsizevalue').textContent;
 
 function makeGrid(x, y) {
     for (let i = 0; i < x; i++) {
@@ -27,16 +33,16 @@ function makeGrid(x, y) {
     }
 }
 
-makeGrid(50, 60);
+// generateButton.addEventListener('click', makeGrid(rowSizeValue, columnSizeValue));
+
+
+makeGrid(50, 50);
 
 clearButton.addEventListener('click', () => {
     boxes.forEach(box => box.style.background = 'white');
 });
 
 const boxes = document.querySelectorAll('.box');
-
-
-
 
 
 colorButtons.forEach(button => button.addEventListener('click', (event) => {
@@ -50,7 +56,31 @@ colorButtons.forEach(button => button.addEventListener('click', (event) => {
 
 
 }));
+
 boxes.forEach(box => box.addEventListener('mouseover', addClass,));
+
+backgroundButtons.forEach(button => button.addEventListener('click', (event) => {
+    if (event.target.id == 'rainbowbackground') {
+        boxes.forEach(box => box.style.background = randomRgb());
+    }
+
+    else 
+    
+    boxes.forEach(box => box.style.background = event.target.innerHTML.toLowerCase());
+} )
+)
+
+addEventListener('keydown', (event) => {
+    if (event.code == 'Space') {
+        boxes.forEach(box => box.removeEventListener('mouseover', addClass));
+    }
+
+    else if (event.code == 'KeyD') {
+        boxes.forEach(box => box.addEventListener('mouseover', addClass));
+    }
+
+    else return;
+});
 
 function addClass() {
     this.style.background = color;
@@ -60,5 +90,7 @@ function randomRgb() {
     let red = Math.floor(Math.random() * 255) + 1;
     let green = Math.floor(Math.random() * 255) + 1;
     let blue = Math.floor(Math.random() * 255) + 1;
-    color = `rgb(${red}, ${green}, ${blue})`;
+    return color = `rgb(${red}, ${green}, ${blue})`;
 }
+
+
